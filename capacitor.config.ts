64ddc7f4f3804e23f24f609dff9a1d9ea2fca27c
dwanejimenez <dwane_jimenez@bhd.com.do>
2026-03-24 17:45:11 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ideas Restaurant',
+  webDir: 'www'
+};
+
+export default config;
